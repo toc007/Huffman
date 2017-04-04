5e97133dbc558168af8c6e9f6e7efba6574a9e1e
Tommy Chen <toc007@ucsd.edu>
2017-04-04 14:04:17 -0700

    Encoder updated

--- src/com/company/Encoder.java
+++ src/com/company/Encoder.java
@@ -8,38 +8,68 @@ import java.util.Scanner;
  */
 
 public class Encoder {
+    private PrintWriter fout;
+    private BufferedReader fin;
+    private int hist[] = new int[256];
 
-    public static void main(String[] args) {
+    public boolean openInputFile() {
         Scanner cin = new Scanner(System.in);
         System.out.println("File to encode: ");
         String inFile = cin.next();
-        System.out.println("Output File: ");
-        String outFile = cin.next();
 
-        // handles reading/writing to output file
-        PrintWriter fout;
-        BufferedReader fin;
-
-        //
         try {
             fin = new BufferedReader(new FileReader(inFile));
-            fout = new PrintWriter(outFile);
-            int hist[] = new int[256];
-            while (true) {
-                int charIn = fin.read();
-                if (charIn == -1)
-                    break;
-                hist[charIn]++;
-                System.out.println((char) charIn);
-            }
-        } catch (FileNotFoundException E) {
-            System.err.println("Output file open failed!!");
-            return;
         } catch (Exception E) {
-            System.err.println("Something fucked up");
+            System.err.println("Input file open fucked up");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean outputInputFile() {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Output File: ");
+        String outFile = cin.next();
+        try {
+            fout = new PrintWriter(outFile);
+        } catch (Exception E) {
+            System.err.println("Output file open fucked up");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void getFrequency() {
+        try {
+            while(true) {
+                int c = fin.read();
+                if(c == -1) break;
+                hist[c]++;
+            }
+            fin.reset();
+        } catch (Exception E) {
+            System.err.println("Reading from input file failed!!");
             return;
         }
     }
 
+    public void resetFrequency() {
+        hist = new int[256];
+    }
 
+    public void printFrequency() {
+        System.out.print("[");
+        for(int i=0; i<256; i++) {
+            System.out.print(hist[i]);
+            if(i!=255) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        Encoder e = new Encoder();
+        while(!e.openInputFile());
+    }
 }
